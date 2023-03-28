e3230508f50031bd2b1ae670e61a95c16cd2e56c
Tishtito <127747198+Tishtito@users.noreply.github.com>
2023-03-28 16:34:07 +0300

    Add files via upload

---
+++ area.py
@@ -0,0 +1,8 @@
+a = float(input("The first height: "))
+b = float(input("The second height: "))
+
+class calculation:
+    def answer(self,a ,b):
+        return 1/2*a*b
+c = calculation()
+print(c.answer(a,b))
